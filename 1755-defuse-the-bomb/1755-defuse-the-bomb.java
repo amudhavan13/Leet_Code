3bbafdff9aa493f3c702dafc88759fb668865caa
Amudhavan M <125903460+amudhavan13@users.noreply.github.com>
2024-09-25 11:54:16 +0530

    Time: 2 ms (58.35%), Space: 42 MB (73.02%) - LeetHub

---
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] a = new int[n];
+        if (k == 0) {
+            return a;
+        } else if (k > 0) {
+            for (int i = 0; i < n; i++) {
+                int s = 0;
+                for (int j = 1; j <= k; j++) {
+                    s += code[(i + j) % n];
+                }
+                a[i] = s;
+            }
+        } else if (k < 0) {
+            for (int i = 0; i < n; i++) {
+                int s = 0;
+                for (int j = 1; j <= -1 * k; j++) {
+                    s += code[(i - j + n) % n];
+                }
+                a[i] = s;
+            }
+        }
+        return a;
+    }
+}
