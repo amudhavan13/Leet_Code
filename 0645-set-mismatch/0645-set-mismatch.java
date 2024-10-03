dc38643d43ed9cc96f81a1d08131832e575de43c
Amudhavan M <125903460+amudhavan13@users.noreply.github.com>
2024-10-03 19:27:29 +0530

    Time: 17 ms (10.74%), Space: 45.5 MB (45.38%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int[] a = new int[2];
+        int j=0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                a[j] = nums[i];
+                j++;
+            }
+        }
+        Set<Integer> set = new HashSet<>();
+        for(int i:nums)
+        {
+            set.add(i);
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(!set.contains(i))
+            {
+                a[j]=i;
+                j++;
+            }
+        }
+        return a;
+        
+    }
+}
